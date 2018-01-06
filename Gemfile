88e19c91e9432844a53f4e0d487fedf1f75d07ad
cheezenaan <cheezenaan@gmail.com>
2018-01-06 15:22:39 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) do |repo_name|
+  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
+  "https://github.com/#{repo_name}.git"
+end
+
+gem "jbuilder", "~> 2.5"
+gem "mysql2", ">= 0.3.18", "< 0.5"
+gem "puma", "~> 3.7"
+gem "rails", "~> 5.1.4"
+gem "sass-rails", "~> 5.0"
+gem "uglifier", ">= 1.3.0"
+
+group :development, :test do
+  gem "byebug", platforms: %i[mri mingw x64_mingw]
+  gem "hirb"
+  gem "pry-byebug"
+  gem "pry-coolline"
+  gem "pry-doc"
+  gem "pry-rails"
+  gem "pry-stack_explorer"
+  gem "rails-flog", require: "flog"
+  gem "spring-commands-rspec"
+end
+
+group :development do
+  gem "better_errors"
+  gem "binding_of_caller"
+  gem "brakeman"
+  gem "bullet"
+  gem "listen", ">= 3.0.5", "< 3.2"
+  gem "rubocop", require: false
+  gem "rufo"
+  gem "spring"
+  gem "spring-watcher-listen", "~> 2.0.0"
+  gem "web-console", ">= 3.3.0"
+end
+
+group :test do
+  gem "capybara", require: false
+  gem "capybara-screenshot"
+  gem "database_cleaner"
+  gem "factory_bot_rails"
+  gem "guard"
+  gem "guard-rspec", require: false
+  gem "rspec-its"
+  gem "rspec-rails"
+  gem "selenium-webdriver", require: false
+  gem "simplecov"
+end
---
+++ config/application.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require_relative "boot"
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module App
+  class Application < Rails::Application
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framekwork :rspec,
+                        fixture: true,
+                        view_specs: false,
+                        helper_specs: false,
+                        routing_specs: false,
+                        controller_specs: false,
+                        request_specs: true
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
+
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    config.time_zone = "Asia/Tokyo"
+
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+  end
+end
