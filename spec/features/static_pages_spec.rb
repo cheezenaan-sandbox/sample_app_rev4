5dcccd1bb1247b1b92866d9d021e2767c873de91
cheezenaan <cheezenaan@gmail.com>
2018-01-06 19:18:39 +0900

    Add tests for static_pages to examine title

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -7,17 +7,29 @@ RSpec.feature "StaticPages", type: :feature do
     before do
       visit static_pages_home_url
     end
+
+    scenario "shows title" do
+      expect(page).to have_title "Home | Ruby on Rails Tutorial Sample App"
+    end
   end
 
   feature "Help" do
     before do
       visit static_pages_help_url
     end
+
+    scenario "shows title" do
+      expect(page).to have_title "Help | Ruby on Rails Tutorial Sample App"
+    end
   end
 
   feature "About" do
     before do
       visit static_pages_about_url
+    end
+
+    scenario "shows title" do
+      expect(page).to have_title "About | Ruby on Rails Tutorial Sample App"
     end
   end
 end
