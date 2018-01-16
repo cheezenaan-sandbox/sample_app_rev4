50d362bf9bb7c419bd42633cb727450dc7e45b78
cheezenaan <cheezenaan@gmail.com>
2018-01-17 08:27:25 +0900

    Introduce eslint-config-airbnb

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -1,21 +1,21 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: { "frontend/application": ["./src/javascripts/application/index.js"] },
+  entry: { 'frontend/application': ['./src/javascripts/application/index.js'] },
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "../../public/assets")
+    filename: '[name].js',
+    path: path.resolve(__dirname, '../../public/assets'),
   },
   module: {
     rules: [
       {
         test: /.js$/,
         exclude: /node_modules|bower_components/,
-        loader: "babel-loader",
-      }
-    ]
+        loader: 'babel-loader',
+      },
+    ],
   },
   resolve: {
-    extensions: [".js", ".jsx"]
-  }
+    extensions: ['.js', '.jsx'],
+  },
 };
