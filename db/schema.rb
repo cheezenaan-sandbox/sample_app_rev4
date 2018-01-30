e678640ae7fd95657e17b7c75c09e2cbf3a70f98
cheezenaan <cheezenaan@gmail.com>
2018-01-31 08:25:03 +0900

    Add unique index to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180129225735) do
+ActiveRecord::Schema.define(version: 20180130232037) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
