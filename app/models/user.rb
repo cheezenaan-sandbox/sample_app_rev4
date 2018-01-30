7d65c3a8a2dd5a8f4e7b78d2c4378b94f8b1bb93
cheezenaan <cheezenaan@gmail.com>
2018-01-30 09:06:52 +0900

    Validate presence for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,38 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  let(:user) { User.new(name: "Kumiko Oumae", email: "anime-eupho@example.com") }
+  let(:user) { User.new(name: name, email: email) }
   subject { user }
 
   describe "#name" do
-    it { is_expected.to be_valid }
-    its(:name) { is_expected.to eq "Kumiko Oumae" }
+    let(:email) { "anime-eupho@example.com" }
+
+    context "when name is blank" do
+      let(:name) { "" }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when name is present" do
+      let(:name) { "Kumiko Oumae" }
+
+      it { is_expected.to be_valid }
+      its(:name) { is_expected.to eq "Kumiko Oumae" }
+    end
   end
 
   describe "#email" do
-    it { is_expected.to be_valid }
-    its(:email) { is_expected.to eq "anime-eupho@example.com" }
+    let(:name) { "Kumiko Oumae" }
+
+    context "when email is blank" do
+      let(:email) { "" }
+      it { is_expected.to be_invalid }
+    end
+
+    context "when email is present" do
+      let(:email) { "anime-eupho@example.com" }
+      it { is_expected.to be_valid }
+      its(:email) { is_expected.to eq "anime-eupho@example.com" }
+    end
   end
 end
