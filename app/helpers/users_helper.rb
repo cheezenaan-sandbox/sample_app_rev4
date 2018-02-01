87892797f5ffa2a46c5d5f5b993e1c0e5783ec7c
cheezenaan <cheezenaan@gmail.com>
2018-02-01 10:04:24 +0900

    Tweak UsersHelper::gravatar_for
    - Enable to specify avatar image size

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 module UsersHelper
-  def gravatar_for(user)
+  def gravatar_for(user, size: 80)
     return unless user&.email.present?
 
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = Pathname.new("https://secure.gravatar.com/avatar/").join(gravatar_id)
+    gravatar_url = Pathname.new("https://secure.gravatar.com/avatar/")
+                           .join(gravatar_id, "?s=#{size}")
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -12,23 +12,35 @@ RSpec.describe UsersHelper do
       it { is_expected.to eq nil }
     end
 
-    context "when user.email is blank" do
+    context "when user is present" do
       let(:user) do
-        User.new(name: "Kumiko Oumae", email: "",
+        User.new(name: "Kumiko Oumae", email: email,
                  password: "euphonium", password_confirmation: "euphonium")
       end
 
-      it { is_expected.to eq nil }
-    end
+      context "email is blank" do
+        let(:email) { "" }
 
-    context "when user.email is NOT blank" do
-      let(:user) do
-        User.new(name: "Kumiko Oumae", email: "anime-eupho@example.com",
-                 password: "euphonium", password_confirmation: "euphonium")
+        it { is_expected.to eq nil }
       end
 
-      it { is_expected.to include user.name }
-      it { is_expected.to include Digest::MD5.hexdigest(user.email) }
+      context "email is present" do
+        let(:email) { "anime-eupho@example.com" }
+        let(:default_size) { 80 }
+
+        it { is_expected.to include user.name }
+        it { is_expected.to include Digest::MD5.hexdigest(user.email) }
+        it { is_expected.to include "s=#{default_size}" }
+      end
+
+      context "when size is specified" do
+        subject { gravatar_for(user, size: size) }
+
+        let(:email) { "anime-eupho@example.com" }
+        let(:size) { 334 }
+
+        it { is_expected.to include "s=#{size}" }
+      end
     end
   end
 end
