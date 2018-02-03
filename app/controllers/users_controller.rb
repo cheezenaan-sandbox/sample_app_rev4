eabc57c5c4b8fed89f09895f02e6d04251c5f2e0
cheezenaan <cheezenaan@gmail.com>
2018-02-04 08:50:48 +0900

    Implement successful signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      redirect_to @user
+      flash[:success] = "Welcome to the Sample App!!!!"
     else
       render "new"
     end
