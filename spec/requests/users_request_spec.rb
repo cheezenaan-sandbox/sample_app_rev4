c343c24d8808490c2ce9d5466db013e776343e19
cheezenaan <cheezenaan@gmail.com>
2018-02-03 11:42:05 +0900

    Add spec for signup request

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-describe "Users", type: :request do
+RSpec.describe "/users", type: :request do
   describe "GET /signup" do
     subject { response }
 
@@ -11,5 +11,33 @@ describe "Users", type: :request do
     end
 
     it { is_expected.to have_http_status(200) }
+  end
+
+  describe "POST /signup" do
+    subject(:signup_request) { -> { post signup_path, params: params } }
+
+    let(:params) do
+      {
+        user: {
+          name: name,
+          email: email,
+          password: password,
+          password_confirmation: password,
+        },
+      }
+    end
+
+    before do
+      signup_request.call
+    end
+
+    context "with invalid information" do
+      let(:name) { "" }
+      let(:email) { "some@foo" }
+      let(:password) { "eupho" }
+
+      it { is_expected.to render_template(:new) }
+      it { is_expected.not_to change(User, :count) }
+    end
   end
 end
