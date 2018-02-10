19a8bfb9e85aee043d0a42b6654987f052c09052
cheezenaan <cheezenaan@gmail.com>
2018-02-11 00:03:57 +0900

    Refactor system specs

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -11,10 +11,13 @@ RSpec.describe "StaticPages", type: :system do
     before { visit root_path }
 
     it { is_expected.to have_title full_title }
-    it { is_expected.to have_link "Home", href: root_path }
-    it { is_expected.to have_link "Help", href: help_path }
-    it { is_expected.to have_link "About", href: about_path }
-    it { is_expected.to have_link "Contact", href: contact_path }
+
+    it "has layout links" do
+      expect(page).to have_link "Home", href: root_path
+      expect(page).to have_link "Help", href: help_path
+      expect(page).to have_link "About", href: about_path
+      expect(page).to have_link "Contact", href: contact_path
+    end
   end
 
   describe "Help page" do
