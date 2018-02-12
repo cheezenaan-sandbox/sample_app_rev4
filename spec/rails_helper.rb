5653f941477e84d51432565bee8f1fdc9378e141
cheezenaan <cheezenaan@gmail.com>
2018-02-13 08:16:35 +0900

    Fix request spec for login with remembering

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,4 +65,5 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include ApplicationHelper, type: :system
+  config.include Sessions::Test::RequestHelper, type: :request
 end
---
+++ spec/support/sessions/request_helper.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Sessions
+  module Test
+    module RequestHelper
+      def log_in_as(user, remember_me: "1")
+        post login_path, params: {
+          session: {
+            email: user.email,
+            password: user.password,
+            remember_me: remember_me,
+          },
+        }
+      end
+    end
+  end
+end
