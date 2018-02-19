e68e1523dc460aaf119d1f13d9c250ce9b5bc96f
cheezenaan <cheezenaan@gmail.com>
2018-02-20 07:42:16 +0900

    Introduce will_paginate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: %i[index edit update]
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def show
