e6efdf0527145f2ef7ea5eee64b7eb7d51f0ff5f
cheezenaan <cheezenaan@gmail.com>
2018-02-21 09:09:56 +0900

    Fix system spec for user index page

--- spec/system/user_index_spec.rb
+++ spec/system/user_index_spec.rb
@@ -11,23 +11,45 @@ RSpec.describe "User index", type: :system do
     it { is_expected.to have_current_path login_path }
   end
 
-  context "when logged in" do
-    let(:user) { FactoryBot.create(:user) }
+  context "when loggged in as an existing user" do
     let(:user_count) { 30 }
 
     before do
       FactoryBot.create_list(:user, user_count, :dummy)
-      log_in_as(user)
-      visit users_path
     end
 
-    it "show user list with pagination" do
-      expect(page).to have_current_path users_path
-      expect(page).to have_selector ".pagination"
-      expect(all("ul.users li").size).to eq user_count
+    context "as an admin" do
+      let(:admin) { FactoryBot.create(:user, :admin) }
 
-      (1..user_count).each do |n|
-        expect(page).to have_link "User #{n}"
+      before { log_in_as(admin) }
+
+      it "show user list with pagination" do
+        visit users_path
+        expect(page).to have_current_path users_path
+        expect(page).to have_selector ".pagination"
+        expect(all("ul.users li").size).to eq user_count
+        expect(page).to have_link "Delete"
+
+        (1..user_count).each do |n|
+          expect(page).to have_link "User #{n}"
+        end
+      end
+    end
+
+    context "as a non-admin" do
+      let(:non_admin) do
+        FactoryBot.create(
+          :user,
+          name: "Asuka Tanaka",
+          email: "anime2@example.com",
+        )
+      end
+
+      before { log_in_as(non_admin) }
+
+      it "show user list without delete link" do
+        visit users_path
+        expect(page).not_to have_link "Delete"
       end
     end
   end
