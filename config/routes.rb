b04d9197b36aacd889614fb93c0a75d847a99c50
cheezenaan <cheezenaan@gmail.com>
2018-02-22 07:47:25 +0900

    Create Users::AccountActivationsController

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
   resources :users
+  namespace :users do
+    resources :account_activations, only: :edit
+  end
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
