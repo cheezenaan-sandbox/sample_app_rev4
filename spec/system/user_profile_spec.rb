dc63bc57d8a249de2c28c3ca7adc633958e69eb1
cheezenaan <cheezenaan@gmail.com>
2018-04-23 08:38:22 +0900

    Add system spec for user profile page

---
+++ spec/system/user_profile_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "User profile", type: :system do
+  subject { page }
+
+  let(:user) { FactoryBot.create(:user, :activated, :with_microposts) }
+
+  context "when access to profile page" do
+    before do
+      visit user_path(user)
+    end
+
+    it "show profile with microposts" do
+      expect(page).to have_title full_title(user.name)
+      expect(page).to have_selector "h1", text: user.name
+      expect(page).to have_selector "h1>img.gravatar"
+      expect(page).to have_selector ".pagination"
+    end
+  end
+end
