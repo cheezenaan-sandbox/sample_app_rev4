d352aa2a8cfe5e7a5e24726e4209e051d021be09
cheezenaan <cheezenaan@gmail.com>
2018-04-27 07:08:59 +0900

    Implement MicropostController#create

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,9 +3,23 @@
 class MicropostsController < ApplicationController
   before_action :authenticate_user!, only: %w[create destroy]
 
-  # TODO
-  def create; end
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+
+    if @micropost.save
+      flash[:success] = "Micropost created!!!"
+      redirect_to root_url
+    else
+      render "static_pages/home"
+    end
+  end
 
   # TODO
   def destroy; end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content)
+  end
 end
