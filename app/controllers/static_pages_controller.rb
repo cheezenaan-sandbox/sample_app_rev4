2c8657f8c55533756496892bb69fe8f9f764215d
cheezenaan <cheezenaan@gmail.com>
2018-04-27 07:30:34 +0900

    Implement micropost form template

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    @micropost = current_user.microposts.build if logged_in?
+  end
 
   def help; end
 
