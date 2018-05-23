d71778a8044ce0e7f13bab7ec623197244c9a460
cheezenaan <cheezenaan@gmail.com>
2018-05-24 07:57:12 +0900

    Add 'following' relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id", dependent: :destroy
 
+  has_many :following, through: :active_relationships, source: :followed
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
