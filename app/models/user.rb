5083107c5a07608376e26ab4470b0ccc9b28744e
cheezenaan <cheezenaan@gmail.com>
2018-05-24 08:18:12 +0900

    Add 'passive_relationships' to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id", dependent: :destroy
 
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id", dependent: :destroy
+
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
@@ -70,5 +74,9 @@ class User < ApplicationRecord
 
   def following?(other)
     following.include?(other)
+  end
+
+  def followed?(other)
+    followed.include?(other)
   end
 end
