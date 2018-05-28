43b0c7ea7338d4e51ef52a80cc5351b98e8fcb51
cheezenaan <cheezenaan@gmail.com>
2018-05-29 08:41:48 +0900

    Add TODO comment
    * [TODO] Enable to follow/unfollow user through Ajax request

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,12 +3,14 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
+  # TODO: Enable to follow user through Ajax request
   def create
     user = User.find(params[:followed_id])
     current_user.follow(user) unless current_user.following?(user)
     redirect_to user
   end
 
+  # TODO: Enable to unfollow user through Ajax request
   def destroy
     user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
